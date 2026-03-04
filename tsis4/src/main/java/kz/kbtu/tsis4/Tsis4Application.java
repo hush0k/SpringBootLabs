5e68508b2873b0031e0036f15800dba25d4abc23
Kanysh <kanyshomirzak@gmai.com>
2026-03-05 01:02:29 +0500

    Add validation to event creation and update endpoints, and initialize project configuration files

---
+++ tsis4/src/main/java/kz/kbtu/tsis4/Tsis4Application.java
@@ -0,0 +1,13 @@
+package kz.kbtu.tsis4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tsis4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tsis4Application.class, args);
+	}
+
+}
--- tsis4/src/main/java/kz/kbtu/tsis4/controller/EventController.java
+++ tsis4/src/main/java/kz/kbtu/tsis4/controller/EventController.java
@@ -1,11 +1,13 @@
 package kz.kbtu.tsis4.controller;
 
+import jakarta.validation.Valid;
 import kz.kbtu.tsis4.dto.request.EventCreateDto;
 import kz.kbtu.tsis4.dto.request.EventUpdateDto;
 import kz.kbtu.tsis4.dto.response.EventResponseDto;
 import kz.kbtu.tsis4.service.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,19 +16,20 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/events")
 @RequiredArgsConstructor
+@Validated
 public class EventController {
     private final EventService eventService;
 
     //CREATE EVENT
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public EventResponseDto createEvent(@RequestBody EventCreateDto dto){
+    public EventResponseDto createEvent(@Valid @RequestBody EventCreateDto dto){
         return eventService.create(dto);
     }
 
     //PATCH EVENT
     @PatchMapping("/{id}")
-    public EventResponseDto updateEvent(@PathVariable UUID id, @RequestBody EventUpdateDto dto){
+    public EventResponseDto updateEvent(@PathVariable UUID id, @Valid @RequestBody EventUpdateDto dto){
         return eventService.updateEvent(id, dto);
     }
 
